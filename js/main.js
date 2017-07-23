9ae1a7aa7a16d382da5f43eb3efa01ab446c7a8c
caroquintana <caro.quintana.n@gmail.com>
2017-07-23 17:30:05 -0400

    avances sección cuatro, cinco, seis, siete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-/*$(document).scroll(function() { 
+$(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
                 if(scroll_pos > 210) {
-                    $("body").css('background-color', 'blue');
+                    $("nav").css('background-color', 'white');
+                    $(".navbar > .container .navbar-brand").css('color', '#555555');
+                    $(".navbar-default .navbar-nav > li > a").css('color', '#555555');
+                    $("a.buytext").css('color', '#fff');
                 } else {
-                    $("body").css('background-color', 'red');
+                    $("nav").css('background-color', 'transparent');
+                    $(".navbar > .container .navbar-brand").css('color', '#fff');
+                    $(".navbar-default .navbar-nav > li > a").css('color', '#fff');
+                    $("a.buytext").css('color', '#fff');
                 }
-            });*/
+            });
