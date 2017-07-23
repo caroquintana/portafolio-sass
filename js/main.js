c9b4825bfafa0a4b4290c10d443a980380efbfa0
caroquintana <caro.quintana.n@gmail.com>
2017-07-22 21:31:23 -0400

    avances en carrusel y nav

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*$(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 210) {
+                    $("body").css('background-color', 'blue');
+                } else {
+                    $("body").css('background-color', 'red');
+                }
+            });*/
